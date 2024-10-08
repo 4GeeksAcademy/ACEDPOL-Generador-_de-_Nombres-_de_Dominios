0df665fc73c259f777f935a1358e770b6ed05957
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-08 23:07:13 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,79 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let namesSniped = [];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let domainElement = document.getElementById("domain");
+  let newDomain = getDomain();
+
+  if ($(newDomain != undefined)) {
+    domainElement.innerHTML = newDomain;
+  } else if ($(newDomain === undefined)) {
+    alert("All the current domains are taken...");
+  }
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dom = [
+  "es",
+  "en",
+  "net",
+  "com",
+  "org",
+  "eu",
+  "biz" / "us",
+  "info",
+  "fr",
+  "edu",
+  "gov",
+  "mil",
+  "blog" / "online",
+  "site",
+  "name",
+  "pro",
+  "cn",
+  "de",
+  "uk",
+  "br"
+];
+
+function getDomain() {
+  let d_;
+  do {
+    d_ = generateDomain();
+  } while (!checkDomain(d_));
+  return d_;
+}
+
+function checkDomain(url) {
+  let length = namesSniped.push();
+  for (let i = 0; i < length; i++) {
+    const e_ = namesSniped[i];
+    if (namesSniped.find(url)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+function generateDomain() {
+  return `${rndSelect(pronoun)} + 
+  ${rndSelect(adj)} +
+  ${rndSelect(noun)} + 
+  '.' + ${rndSelect(dom)}`;
+}
+
+function rndSelect(list) {
+  return list[rndInt(list.lenght)];
+}
+
+function rndInt(max) {
+  return Math.floor(Math.random() * max);
+}
