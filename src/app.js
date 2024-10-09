443eb2445c6bbf6296e3239a7010e9220259021e
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-09 02:15:34 +0000

    feature: button reset

--- src/app.js
+++ src/app.js
@@ -5,25 +5,47 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// variables constantes
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dom = [
+  "com", // Global
+  "es", // Epaña
+  "eu" // Europa
+];
+const MAX_ODDS = pronoun.length * adj.length * noun.length * dom.length;
+
 // variables que se mantienen en el tiempo
 let namesSniped = [];
 let contElement = document.getElementById("creations");
+let domainElement = document.getElementById("domain");
 
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-  // Sets the generator button functionality
+  // Sets generator button feature
   let btn = document.getElementById("btnNewDomain");
   btn.addEventListener("click", () => newDomain());
 
+  // Sets reset button feature
+  let btn2 = document.getElementById("btnReset");
+  btn2.addEventListener("click", () => reset());
+
   // Creates and show the new domain generated that is not taken
+  domainElement.innerHTML = "default_blank.url";
   newDomain();
 };
 
+function reset() {
+  namesSniped = [];
+  contElement.innerHTML = namesSniped.length + " / " + MAX_ODDS;
+  domainElement.innerHTML = "default_blank.url";
+}
+
 function newDomain() {
   // Front end
-  let domainElement = document.getElementById("domain");
   let newDomain = getDomain();
 
   // Backend
@@ -33,6 +55,9 @@ function newDomain() {
       "It seems that it is not available, please try again!";
   } else {
     // console.log("Search domain completed!");
+    if (namesSniped.length !== 1) {
+      contElement.innerHTML = namesSniped.length - 1 + " / " + MAX_ODDS;
+    }
     domainElement.innerHTML = newDomain;
   }
 
@@ -41,6 +66,16 @@ function newDomain() {
 
 // Main function of this script finality ~ 'Engine system' a.k.a. Acedpol
 function getDomain() {
+  // Initial red flag
+  if (namesSniped.length === 0) {
+    //console.log("Exit by zero! (First domain created)");
+    let dom = "default_blank.url";
+    contElement.innerHTML = namesSniped.length + " / " + MAX_ODDS;
+    namesSniped.push(dom);
+    return dom;
+  }
+
+  // Start point marker
   console.log("____Generator running____");
 
   // Main variables
@@ -62,30 +97,22 @@ function getDomain() {
   }
 
   // Response
-  contElement.innerHTML = namesSniped.length;
   return d_;
 }
 
-function takenDomain(url) {
+function takenDomain(dom) {
   //console.log("Names actually in use: " + namesSniped.length);
-
-  if (namesSniped.length === 0) {
-    //console.log("Exit by zero! (First domain created)");
-    namesSniped.push(url);
-    return false;
-  }
-
   for (let i = 0; i < namesSniped.length; i++) {
     const e_ = namesSniped[i];
     // console.log(
     //   "Entra en el if() -- (Comprueba si el dominio generado se encuentra disponible)"
     // );
-    if (namesSniped.some(e_ => e_ === url)) {
+    if (namesSniped.some(e_ => e_ === dom)) {
       //console.log("Este dominio NO se encuentra disponible: " + e_);
       return true;
     } else {
       //console.log("DIRECCION DISPONIBLE");
-      namesSniped.push(url); // returns lenght
+      namesSniped.push(dom); // returns lenght
       return false;
     }
   }
@@ -108,14 +135,6 @@ function rndInt(max) {
   // console.log("random number = " + n);
   return Math.floor(Math.random() * max);
 }
-
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let dom = [
-  "com", // Global
-  "es" // Epaña
-];
 
 // Otras extensiones:
 //   "net", // Es una de las más antiguas y fue originalmente creada para sitios relacionados con redes (de ahí su nombre, “network”).
