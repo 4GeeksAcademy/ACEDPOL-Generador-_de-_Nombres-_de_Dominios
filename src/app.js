70111e943bdc2705d54878a0fee010a4c36e738a
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-10-09 18:01:42 +0000

    feature: cambio de colores cuando se completa

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ import "./assets/img/4geeks.ico";
 // variables para elementos html
 let contElement = document.getElementById("creations");
 let domainElement = document.getElementById("domain");
+let btn = document.getElementById("btnNewDomain");
+let btn2 = document.getElementById("btnReset");
 
 // variables constantes
 let pronoun = ["the", "our"];
@@ -15,10 +17,21 @@ let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
 let dom = [
   "com", // Global
-  "es", // Epaña
-  "eu" // Europa
+  "es" // Epaña
 ];
 const MAX_ODDS = pronoun.length * adj.length * noun.length * dom.length;
+const MAIN_CLASSNAME = "col-2 mb-2 inline-block position-relative";
+const BTN = "btn btn-custom-cursor";
+const DOMAIN = "col-6 inline-block mx-auto display_domain";
+const DOMAIN_ON = "text-black";
+const DOMAIN_OFF = "text-secondary";
+const ON_COLORS = "bg-success text-light";
+const OFF_COLORS = "bg-body-secondary text-black";
+const BTN_SPAN = `<span
+                id="creations"
+                class="fs-6 ms-2 text-light text-body-primary-emphasis position-absolute top-50 start-100 translate-middle-y badge rounded-pill bg-danger"
+                >OB1+ <span class="visually-hidden">Stack overflow</span></span
+              >`;
 
 // variables que se actualizan en el tiempo
 let namesSniped = [];
@@ -27,12 +40,26 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
-  // Sets generator button feature
-  let btn = document.getElementById("btnNewDomain");
-  btn.addEventListener("click", () => newDomain());
+  // Sets button GENERATE feature
+  btn.addEventListener("click", () => {
+    // HINT: Añadir disable para que no se pueda pulsar más el botón de generar dominio
+    // al alcanzar el número MAX_ODDS en la lista namesSniped.
+    if (namesSniped.length <= MAX_ODDS) {
+      domainElement.className = DOMAIN + " " + DOMAIN_ON;
+      // MAIN LOOP
+      newDomain();
+    } else if (!btn.className.includes("disable", MAIN_CLASSNAME.length)) {
+      // DESACTIVA BTN
+      btn.className = BTN + " " + MAIN_CLASSNAME + " disabled " + OFF_COLORS;
+      btn.innerHTML = "Limit reached!!" + BTN_SPAN;
+      contElement = document.getElementById("creations"); // vuelve de cero
+      domainElement.className = DOMAIN + " " + DOMAIN_OFF;
+      // FULL list
+      contElement.innerHTML = "OB1+";
+    }
+  });
 
-  // Sets reset button feature
-  let btn2 = document.getElementById("btnReset");
+  // Sets button RESET feature
   btn2.addEventListener("click", () => reset());
 
   // Creates and show the new domain generated that is not taken
@@ -41,6 +68,12 @@ window.onload = function() {
 
 function reset() {
   namesSniped = [];
+  // ACTIVA BTN
+  btn.className = BTN + " " + MAIN_CLASSNAME + " " + ON_COLORS;
+  btn.innerHTML = "Create new domain" + BTN_SPAN;
+  contElement = document.getElementById("creations"); // vuelve de cero
+  domainElement.className = DOMAIN + " " + DOMAIN_OFF;
+  // MAIN LOOP
   newDomain();
 }
 
@@ -48,7 +81,7 @@ function newDomain() {
   // Backend
   console.clear();
   let newDomain = getDomain();
-  namesSniped.push(newDomain);
+  namesSniped.push(newDomain); // +01
 
   // Front end
   if (newDomain === undefined) {
@@ -81,16 +114,16 @@ function getDomain() {
   }
 
   // Main variables
-  let d_,
+  let DOM,
     cont = 0;
   const MAX = MAX_ODDS * 10; // number of oportunities
 
   // Main loop
   do {
     cont++;
-    d_ = generateDomain();
-    console.log(d_);
-  } while (takenDomain(d_) && cont < MAX);
+    DOM = generateDomain();
+    console.log(DOM);
+  } while (takenDomain(DOM) && cont < MAX);
 
   // Time limit
   if (cont === MAX) {
@@ -98,7 +131,7 @@ function getDomain() {
   }
 
   // Response
-  return d_;
+  return DOM;
 }
 
 function takenDomain(dom) {
